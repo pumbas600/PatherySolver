f2c1ff8f63575fea21030fcd0b49724f5a862f07
pumbas600 <joshjeffers600@gmail.com>
2023-11-09 15:19:16 +1300

    feat: :sparkles: Add a flag for valued tile types

--- src/main/java/net/pumbas/models/TileType.java
+++ src/main/java/net/pumbas/models/TileType.java
@@ -11,7 +11,7 @@ public enum TileType {
   OPEN("o", "Open"),
   START("s", "Start"),
   FINISH("f", "Finish"),
-  CHECKPOINT("c", "Checkpoint"),
+  CHECKPOINT("c", "Checkpoint", true),
   ROCK("r", "Rock"),
   TELEPORT_IN("t", "Teleport In"),
   TELEPORT_OUT("u", "Teleport Out"),
@@ -29,6 +29,21 @@ public enum TileType {
 
   private final String code;
   private final String name;
+
+  /**
+   * Whether this tile has a meaningful value associated with it. A meaningful value is one that
+   * affects the game in some way. For example, rock has different values to change the style of it
+   * in the website, but these are purely visual and don't affect the game, so it is not
+   * meaningful.
+   * <p>
+   * While some tiles may have a value that is meaningful, this will be false if that value is
+   * supported by the solver.
+   */
+  private final boolean isValued;
+
+  TileType(final String code, final String name) {
+    this(code, name, false);
+  }
 
   public static TileType fromCode(final String code) {
     return codeMap.get(code);
--- src/main/java/net/pumbas/models/ValuedTile.java
+++ src/main/java/net/pumbas/models/ValuedTile.java
@@ -10,4 +10,12 @@ public class ValuedTile {
   private final TileType tileType;
   private final int value;
 
+  /**
+   * @param tileType The tile type to compare to
+   * @return {@code true} if this tile is of the given {@link TileType}, {@code false} otherwise
+   */
+  public boolean is(final TileType tileType) {
+    return this.tileType == tileType;
+  }
+
 }
